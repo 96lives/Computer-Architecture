76310cf7a375beba716bb351b3361cbd7f72bdf0
DS-Ubuntu <96lives@gmail.com>
2018-05-01 11:26:20 +0900

    hw3 started

---
+++ hw3/bmpgrid.s
@@ -0,0 +1,46 @@
+#---------------------------------------------------------
+# 
+#  Project #3: Drawing grid lines in an image
+#
+#  April 30, 2018
+#
+#  Jin-Soo Kim (jinsoo.kim@snu.ac.kr)
+#  Systems Software & Architecture Laboratory
+#  Dept. of Computer Science and Engineering
+#  Seoul National University
+#
+#---------------------------------------------------------
+
+
+.text
+	.align 4
+.globl bmp_grid
+	.type bmp_grid,@function
+
+bmp_grid:
+	#------------------------------------------------------------
+	# Use %rax, %rbx, %rcx, %rdx, %rsi, and %rdi registers only
+	#	imgptr is in %rdi
+	#	width  is in %rsi
+	#	height is in %rdx
+	#   gap	   is in %rcx
+	#------------------------------------------------------------
+
+	# --> FILL HERE <--
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	#------------------------------------------------------------
+
+	ret
