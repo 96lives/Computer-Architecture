57dca6507200436df1cac658fc6a9d0d29fb02f7
DS-Ubuntu <96lives@gmail.com>
2018-05-03 08:03:13 +0900

    horizontal finished

--- hw3/bmpgrid.s
+++ hw3/bmpgrid.s
@@ -18,6 +18,36 @@
 	.type bmp_grid,@function
 
 
+# %rbx always has the imgptr
+
+
+
+#------------------------------------------------------
+# %rax: current starting address, %rsi: bitWidth
+# returns the next starting address
+drawHorLine:
+	
+	pushq %rbx
+	# %rbx is the counter that counts till width
+	movq $0, %rbx
+	.drawDotRed:
+		movq $0, (%rax, %rbx)
+		movq $0, 1(%rax, %rbx)
+		movq $255, 2(%rax, %rbx)
+		addq $3, %rbx
+		cmpq %rbx, %rsi
+		jg .drawDotRed
+	cmpq %rbx, %rsi
+	je .done
+	subq $1, %rbx
+	cmpq %rbx, %rsi
+	je .done
+	subq $1, %rbx
+
+	.done:
+		leaq (%rax, %rbx), %rax
+		popq %rbx
+		ret
 
 
 bmp_grid:
@@ -27,39 +57,62 @@ bmp_grid:
 	#	width  is in %rsi
 	#	height is in %rdx
 	#   gap	   is in %rcx
-	#------------------------------------------------------------
+	#-----------------------------------------------------------
 
-	movq $0, (%rdi)
-	movq $0, 1(%rdi)
-	movq $255, 2(%rdi)
-	movq $0, 3(%rdi)
-	movq $0, 4(%rdi)
-	movq $255, 5(%rdi)
-	movq $0, 6(%rdi)
-	movq $0, 7(%rdi)
-	movq $255, 8(%rdi)
-	movq $0, 9(%rdi)
-	movq $0, 10(%rdi)
-	movq $255, 11(%rdi)
+	# make width = 3 * width + pad
+	
+	leaq (%rsi, %rsi, 2), %rsi
+	movq %rsi, %rbx
+	andq $3, %rbx
+	cmpq $0, %rbx
+	jne .notZero
+	movq $4, %rbx
+	.notZero:
+	negq %rbx
+	addq $4, %rbx
+	addq %rbx, %rsi
 
+	# gap = gap + 1
+	leaq 1(%rcx), %rcx
+	
+	# move the pointer to the return value
+	movq %rdi, %rbx
 
+#---------------------------------------------------------
+	# draw rows
+	# %rax keeps the address to start of the row
+	# %rdi keeps the end of the img
+	
+	movq %rsi, %rax
+	pushq %rdx
+	mulq %rdx
+	popq %rdx
+	addq %rax, %rdi
 
+	# Now: %rax=?, %rbx=imgPtr, %rcx=gap, %rdx=height, %rdi=imgEnd, %rsi=bitWidth
+	# Stack: []
+    pushq %rcx
+    pushq %rdx
+    movq %rcx, %rax
+    mulq %rsi
+    movq %rax, %rcx
 
-	movq %rdi, %rax
+    movq %rdi, %rax
+    subq %rsi, %rax
+    popq %rdx
 
+ 	# Now: %rax=imgEnd-bitWidth, %rbx=imgPtr, %rcx=gap*bitWidth, %rdx=height, %rdi=imgEnd, %rsi=bitWidth
+	# Stack: [gap]
 
+    .drawHorLines:
+		call drawHorLine
+		subq %rcx, %rax
+		cmpq %rbx, %rax
+		jge .drawHorLines
 
-
-
-
-
-
-
-
-
-
-
-
+	popq %rcx
+	
+	movq %rbx, %rax
 
 	#------------------------------------------------------------
 
