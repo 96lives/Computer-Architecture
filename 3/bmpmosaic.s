37d99e882ed1a204d064492145b658ffc5423959
96lives <96lives@gmail.com>
2018-11-18 19:24:08 +0900

    using stack

--- 3/bmpmosaic.s
+++ 3/bmpmosaic.s
@@ -28,16 +28,53 @@ bmp_mosaic:
 	#	height is in %rdx
 	#	size   is in %rcx
 	#------------------------------------------------------------
+    pushq %rbx
+    movq %rsi, %rbx
+    andq $3, %rbx
+    leaq (%rsi, $rsi, 2), %rsi
+    addq %rbx, %rsi
+    # rax=?, rbx=width, rcx=size, rdx=height, rsi=bitWidth, rdi=imgPtr
+    pushq %rdi
+    xchq %rdi, %rdx
+    # rax=?, rbx=width, rcx=size, rdx=, rsi=bitWidth, rdi=height
+    # stack: [%rbx, %imgPtr
+    movq %rbx, %rax
+    divq %rcx
+    pushq %rax
+    movq %rdi, %rax
+    divq %rcx
+    pushq %rax
+    # rax=?, rbx=width, rcx=size, rdx=, rsi=bitWidth, rdi=height
+    # stack: [%rbx, %imgPtr, I_MAX, J_MAX
+    movq $0, %rax
+    pushq %rsi
+    # rax=I, rbx=width, rcx=size, rdx=J, rsi=K, rdi=height
+    # stack: [%rbx, %imgPtr, I_MAX, J_MAX, bitWidth
+    LOOP_I:
+        movq $0, %rdx
+        LOOP_J:
+            movq $0, %rsi
+            LOOP_K:
+                incq %rsi
+                cmpq %rsi, $3
+                jl LOOP_J
+            incq %rdx
+            cmpq %rdx, 4(%rsp)
+            jle LOOP_I
+        incq %rax
+        cmpq %rdx, 8(%rsp)
+        jle LOOP_I
+
+CHANGE_PIXELS:
+    pushq %rbx
+
+    pushq %rax
+    pushq %rdx
+    # rax=i, rbx=w, rcx=sum, rdx=j, rsi=bitWidth, rdi=h
 
 
-
-
-	# --> FILL HERE <--
-
-
-
-
-
-
+    popq %rax
+    popq %rdx
 	#------------------------------------------------------------
 	ret
+
