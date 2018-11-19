b1434a7a67f924e79db9d7fc60aed9376940d1ff
96lives <96lives@gmail.com>
2018-11-19 18:54:34 +0900

    getting better

--- 3/bmpmosaic.s
+++ 3/bmpmosaic.s
@@ -40,7 +40,6 @@ bmp_mosaic:
     mulq %rdx
     addq %rdi, %rax
     subq %rsi, %rax
-    DEBUG1:
     # rax=lastRow, %rbx=?, %rcx=size, %rdx=?, %rsi=bitWidth, %rdi=imgPtr
     # stack: [%rbx, width, height
     movq %rax, %rdi
@@ -84,31 +83,34 @@ bmp_mosaic:
 ret
 
 # rax=?, %rbx=J counter, %rcx=size, %rdx=?, %rsi=bitWidth, %rdi=imgPtr
-# stack: [%rbx, width, (decreased)height, MAX_J, bitWidth*size, rowPointer
+# stack: [%rbx, width, (decreased)height, MAX_J, bitWidth*size, rowPointer ,BLUE_SQUARE
 .BLUR_SQUARE:
     # calculate width
     pushq %rbx # push J Counter
     movq %rbx, %rax
     mulq %rcx
-    movq 40(%rsp), %rbx
+    movq 48(%rsp), %rbx
     subq %rbx, %rax
     cmpq %rbx, %rcx
     cmovl %rcx, %rbx # assign size to new_width
     # rax=?, %rbx=new_width, %rcx=size, %rdx=?, %rsi=bitWidth, %rdi=imgPtr
-    # stack: [%rbx, width, (decreased)height, MAX_J, bitWidth*size, rowPointer, J Counter
+    # stack: [%rbx, width, (decreased)height, MAX_J, bitWidth*size, rowPointer,
+    # BLUE_SQUARE, J Counter
     pushq %rsi # push %rsi
-    movq 32(%rsp), %rsi
+    movq 40(%rsp), %rsi
     cmpq %rsi, %rcx
     cmovl %rcx, %rsi # assign size to new_height
     pushq %rcx
     # rax=?, %rbx=new_width, %rcx=?, %rdx=?, %rsi=new_height, %rdi=imgPtr
-    # stack: [%rbx, width, (decreased)height, MAX_J, bitWidth*size, rowPointer, J Counter, bitWidth, size
+    # stack: [%rbx, width, (decreased)height, MAX_J, bitWidth*size, rowPointer,
+    # BLUE_SQUARE, J Counter, bitWidth, size
     movq $0, %rax
     LOOP_k:
         pushq %rdi
         addq %rax, %rdi
         # rax=k, %rbx=new_width, %rcx=?, %rdx=?, %rsi=new_height, %rdi=imgPtr
-        # stack: [%rbx, width, (decreased)height, MAX_J, bitWidth*size, rowPointer, J Counter, bitWidth, size, imgPtr
+        # stack: [%rbx, width, (decreased)height, MAX_J, bitWidth*size, rowPointer,
+        # BLUE_SQUARE, J Counter, bitWidth, size, imgPtr
         jmp .BLUR_ONE_CHANNEL
         .BLUR_ONE_CHANNEL_END:
         incq %rax
@@ -116,7 +118,8 @@ ret
         cmpq $3, %rax
         jl LOOP_k
     # rax=k, %rbx=new_width, %rcx=?, %rdx=?, %rsi=new_height, %rdi=imgPtr
-    # stack: [%rbx, width, (decreased)height, MAX_J, bitWidth*size, rowPointer, J Counter, bitWidth, size
+    # stack: [%rbx, width, (decreased)height, MAX_J, bitWidth*size, rowPointer,
+    # BLUE_SQUARE, J Counter, bitWidth, size
     popq %rcx
     popq %rsi
     popq %rbx
@@ -125,14 +128,16 @@ ret
 
 
 # rax=k, %rbx=new_width, %rcx=?, %rdx=?, %rsi=new_height, %rdi=imgPtr
-# stack: [%rbx, width, (decreased)height, MAX_J, bitWidth*size, rowPointer, J Counter, bitWidth, size, imgPtr
+# stack: [%rbx, width, (decreased)height, MAX_J, bitWidth*size, rowPointer,
+# BLUR_SQUARE, J Counter, bitWidth, size, imgPtr, BLUR_ONE_CHANNEL
 .BLUR_ONE_CHANNEL:
     pushq %rax
     pushq %rbx
     pushq %rsi
     # rax=?, %rbx=x counter, %rcx=y counter, %rdx=?, %rsi=sum, %rdi=imgPtr
     # stack: [%rbx, width, (decreased)height, MAX_J, bitWidth*size, rowPointer,
-    # J Counter, bitWidth, size, imgPtr, k, new_width, new_height
+    # BLUR_SQUARE, J Counter, bitWidth, size, imgPtr,
+    # BLUR_ONE_CHANNEL, k, new_width, new_height
     movq $0, %rsi
     movq $0, %rbx
     LOOP_X_SUM:
@@ -146,9 +151,9 @@ ret
             cmpq 8(%rsp), %rcx
             jl LOOP_Y_SUM
         popq %rdi
-        subq 40(%rsp), %rdi # subtract bitWidth
+        subq 48(%rsp), %rdi # subtract bitWidth
         incq %rbx
-        cmpq 16(%rsp), %rbx
+        cmpq 8(%rsp), %rbx
         jl LOOP_X_SUM
     # rax=?, %rbx=x counter, %rcx=y counter, %rdx=?, %rsi=sum, %rdi=imgPtr
     # stack: [%rbx, width, (decreased)height, MAX_J, bitWidth*size, rowPointer,
@@ -164,7 +169,7 @@ ret
     movq $0, %rbx
     LOOP_X_ASSIGN:
         movq $0, %rcx
-        addq 40(%rsp), %rdi # add bitWidth
+        addq 48(%rsp), %rdi # add bitWidth
         pushq %rdi
         LOOP_Y_ASSIGN:
             movb $255, (%rdi)
@@ -174,7 +179,7 @@ ret
             jl LOOP_Y_ASSIGN
         popq %rdi
         incq %rbx
-        cmpq 16(%rsp), %rbx
+        cmpq 8(%rsp), %rbx
         jl LOOP_X_ASSIGN
     popq %rsi
     popq %rbx
